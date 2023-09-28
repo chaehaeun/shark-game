36880387d00324532c125c413195509b1f5663e8
chaehaeun <chae_3210@naver.com>
2023-09-28 23:24:44 +0900

    [💄] main 페이지 스타일 작업

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,30 @@
-const main = () => {
-  return <div>Hello</div>;
+import { Button } from "@/components";
+import { WORD_LENGTH } from "@/constants";
+import { GiSharkFin } from "react-icons/gi";
+
+const Main = () => {
+  return (
+    <>
+      <h2 className="sr-only">게임 세팅 페이지</h2>
+      <div className="flex flex-col items-center">
+        <GiSharkFin className="w-28 h-28 mx-auto my-11" />
+        <p className="text-center">단어의 길이를 선택해주세요.</p>
+        <select
+          className="border border-black p-2 my-5"
+          title="Word Length"
+          name="wordLength"
+          id="wordLength"
+        >
+          {WORD_LENGTH.map((length) => (
+            <option key={length} value={length}>
+              {length}
+            </option>
+          ))}
+        </select>
+        <Button onClick={() => {}}>게임 시작하기</Button>
+      </div>
+    </>
+  );
 };
 
-export default main;
+export default Main;
