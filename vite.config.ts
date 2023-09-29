bcbee2f31ffb35dea85f7da16ae41b57f9dc44bb
chaehaeun <chae_3210@naver.com>
2023-09-29 22:00:53 +0900

    [🔧 ] 텍스트압축 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), compression()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
