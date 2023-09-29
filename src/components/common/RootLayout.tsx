e41491e7a5b9298e48dddc52373f388b7f8eb658
chaehaeun <chae_3210@naver.com>
2023-09-29 20:55:13 +0900

    [💄] header, footer 간격 조정

--- src/components/common/RootLayout.tsx
+++ src/components/common/RootLayout.tsx
@@ -4,12 +4,14 @@ const RootLayout = () => {
   return (
     <>
       <header>
-        <h1 className=" text-5xl font-bold text-center py-4">Shark Game</h1>
+        <h1 className="py-4 my-5 text-5xl font-bold text-center ">
+          Shark Game
+        </h1>
       </header>
       <main>
         <Outlet />
       </main>
-      <footer className="text-center py-4 mt-10">
+      <footer className="py-4 mt-10 text-center">
         <p>현재 페이지는 PC Chrome 브라우저 환경에 최적화 되어있습니다.</p>
       </footer>
     </>
