642af08dbcd3e1321eab04067c8ac80dc5bc9ed0
chaehaeun <chae_3210@naver.com>
2023-09-29 22:14:50 +0900

    [💄] not found 페이지 스타일링

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,9 @@
 const NotFound = () => {
-  return <div>Page Not Found</div>;
+  return (
+    <h1 className="mt-10 text-3xl font-semibold text-center">
+      잘못된 페이지 접근입니다.
+    </h1>
+  );
 };
 
 export default NotFound;
